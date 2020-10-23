0e75ee484041230d09ac43be78faf851b934ec77
Fabiany Sousa <fabianydesousa@gmail.com>
2020-10-23 14:30:56 -0300

    Adicionando pacotes NuGet e contexto de banco de dados
    Pacote NuGet Microsoft.EntityFrameworkCore.SqlServer e o Contexto do banco esta no TodoContext.

---
+++ Models/TodoContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi.Models
+{
+    // Adicionando contexto de banco de dados
+    public class TodoContext : DbContext
+    {
+        // DbContextOptions : fornece opções... configurações do banco.
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        // DbSeT : possibilidades de manusear as tabelas do banco.
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
