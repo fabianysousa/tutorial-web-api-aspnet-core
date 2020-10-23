11c1219fa3963dfd4918a91fbda95c6a9134f17d
Fabiany Sousa <fabianydesousa@gmail.com>
2020-10-23 14:11:03 -0300

    Criando modelo TodoItem.
    Contém propriedades plublicas Id, Name, IsComplete.

---
+++ Models/TodoItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Models
+{
+    // Model : conjunto de classes que representam os dados gerenciados pelo aplicativo.
+    // As classes de modelo podem ir em qualquer lugar do projeto, mas a Models pasta é usada pela Convenção.
+    public class TodoItem
+    {
+        // Id: chave exclusiva em um banco de dados relacional
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+
+        // Os gets e sets das propriedades são necessários para que possa ser feito o gerenciamento de acesso dessas propriedades.
+    }
+}
